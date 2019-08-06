6c9b1275f69cd79e42158b5b9902f054be3ffdf8
yardenroee <yardenroee@gmail.com>
2019-08-06 18:33:10 -0400

    first commit

---
+++ src/dashboard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+        margin: '50px'
+    },
+    flex: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    weatherWindow: {
+        width: '40%',
+        height: '400px',
+    },
+    mapWindow: {
+        width: '60%',
+        height: '400px',
+    },
+}));
+
+
+
+export default function Dashboard() {
+    const classes = useStyles();
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <Typography variant="h5" component="h3">
+                    Yarden's Weather App
+                </Typography>
+                
+                <div className={classes.flex}>
+                    <div className={classes.weatherWindow}>
+
+                    </div>
+
+                    <div className={classes.mapWindow}>
+
+                    </div>
+                </div>
+            </Paper>
+        </div>
+    )
+}
