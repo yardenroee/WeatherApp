f828047e8a06398f6c839b5cffc96bafd91e298d
yardenroee <yardenroee@gmail.com>
2019-08-08 18:12:46 -0400

    Updates

---
+++ precache-manifest.6263a1054e474aa55b4a7952d9e8c22d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d4579acc0fcb0362183c13c716d493",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "5d97251ca4db4c8b963f",
+    "url": "/WeatherApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ea64aaffa81c64fb04c3",
+    "url": "/WeatherApp/static/js/2.d698a5c3.chunk.js"
+  },
+  {
+    "revision": "5d97251ca4db4c8b963f",
+    "url": "/WeatherApp/static/js/main.57e6e0f5.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  }
+]);
