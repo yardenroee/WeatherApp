0da9b21944cd380d6b58c75873a1f4f560376d8d
yardenroee <yardenroee@gmail.com>
2019-08-08 18:39:41 -0400

    maybe now itll work

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Dashboard from './dashboard';
 import Store from './store';
+import { BrowserRouter as Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <Store>
-      <Dashboard />
+        <Dashboard/>
       </Store>
     </div>
   );
